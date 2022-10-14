68e8217765c301f8b4532b19582878d0279f9a73
alojine <76651648+alojine@users.noreply.github.com>
2022-10-15 01:36:39 +0300

    conttroller fix

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
+// Constants
 const express = require('express');
 
 const fs = require('fs');
+
 const path = require('path');
 
 const app = express();
 
+// Database
+const mongoose = require('mongoose');
+
+// Routes
+const login = require('./controller/login');
+const register = require('./controller/register');
+
+// const mongoSrv = process.env.MONG_SRV;
+
+// Database
+// mongoose.connect(mongoSrv, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => terminal.info('[MONGODB] Connection established!'));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 
 // midleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false}));
 
-// default path
-app.get('/', (req, res) =>{
-    res.render('login');
-});
+// middleware routes
+app.use('/', login);
+app.use('/login', login);
+app.use('/register', register);
 
-// register
-app.get('/register', (req, res) =>{
-    res.render('register');
-});
 
-// login
-app.get('/login', (req, res) =>{
-    res.render('login');
-});
 
 app.listen(3000);
